d843cb5b7ae1a01ce0a3c0390b9fb25002dd763e
Tara Whirley <whirleyt@outlook.com>
2018-02-26 19:16:07 -0500

    Models and associations completed

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,13 +2,23 @@ const User = require('./user')
 const Place = require('./place')
 const Order = require('./order')
 const List = require('./list')
+const Wine = require('./wine')
+const Review = require('./review')
 
 
 
 User.belongsTo(Place, { as: 'place' })
-User.hasMany(Order, { as: 'order' })
+User.hasMany(Order, { as: 'orders' })
+User.hasMany(Review, { as: 'reviews'})
 Order.belongsTo(User, { as: 'user' })
-Order.hasMany(List, { as: 'list' })
+Order.hasMany(List, { as: 'lists' })
+Wine.belongsTo(Place, { as: 'place' })
+Wine.hasMany(List, { as: 'lists' })
+Wine.hasMany(Review, { as: 'reviews'})
+Review.belongsTo(Wine, { as: 'wine' })
+Review.belongsTo(User, { as: 'user' })
+
+
 
 
 
@@ -29,5 +39,7 @@ module.exports = {
   User,
   Place,
   Order,
-  List
+  List,
+  Wine, 
+  Review
 }
