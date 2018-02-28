a64ff22a79aa5c544ddd5bb7d51d6468630ea22f
Kyle Yee <20980584+ShifuYee@users.noreply.github.com>
2018-02-28 13:19:35 -0500

    completed login and signup functionality

--- client/components/signup.js
+++ client/components/signup.js
@@ -4,52 +4,52 @@ import { signup } from '../store'
 
 
 export const Signup = (props) => {
-    return (
+  return (
+    <div>
+      <form onSubmit={props.signupSubmit}>
         <div>
-            <form onSubmit={props.signupSubmit}>
-            <div>
-            <label>First Name:</label>
-            <input
-              name="firstName"
-              type="firstName"
-            />
-          </div>
-          <div>
+          <label>First Name:</label>
+          <input
+            name="firstName"
+            type="firstName"
+          />
+        </div>
+        <div>
           <label>Last Name:</label>
           <input
             name="lastName"
             type="lastName"
           />
         </div>
-              <div>
-                <label>Email:</label>
-                <input
-                  name="email"
-                  type="email"
-                />
-              </div>
-              <div>
-                <label>Password:</label>
-                <input
-                  name="password"
-                  type="password"
-                />
-              </div>
-              <div>
-              <label>Address:</label>
-              <input
-                name="address"
-                type="address"
-              />
-            </div>
-            <div>
-            <label>City:</label>
-            <input
-              name="city"
-              type="city"
-            />
-          </div>
-          <div>
+        <div>
+          <label>Email:</label>
+          <input
+            name="email"
+            type="email"
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            name="password"
+            type="password"
+          />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input
+            name="address"
+            type="address"
+          />
+        </div>
+        <div>
+          <label>City:</label>
+          <input
+            name="city"
+            type="city"
+          />
+        </div>
+        <div>
           <label>State:</label>
           <input
             name="state"
@@ -57,35 +57,43 @@ export const Signup = (props) => {
           />
         </div>
         <div>
-        <label>Phone:</label>
-        <input
-          name="phone"
-          type="phone"
-        />
-      </div>
-              <button type="submit">Sign up</button>
-            </form>
-          </div>
-      );
+          <label>Country:</label>
+          <input
+            name="country"
+            type="country"
+          />
+        </div>
+        <div>
+          <label>Phone:</label>
+          <input
+            name="phone"
+            type="phone"
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 }
 
 
-const mapState = () => ({message: 'Sign up'})
+const mapState = ({ loggedInUser }) => ({ loggedInUser })
 
 const mapDispatch = (dispatch) => ({
-    signupSubmit(event){
-     event.preventDefault()
-     dispatch(signup({
-        firstName: event.target.firstName.value,
-        lastName: event.target.lastName.value,
-        email: event.target.email.value,
-        password: event.target.password.value,
-        address: event.target.address.value,
-        city: event.target.city.value,
-        state: event.target.state.value,
-        phone: event.target.phone.value
-     }))
-    }
+  signupSubmit(event){
+    event.preventDefault()
+    dispatch(signup({
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
+      address: event.target.address.value,
+      city: event.target.city.value,
+      state: event.target.state.value,
+      country: event.target.country.value,
+      phone: event.target.phone.value
+    }))
+  }
 })
 
 export default connect(mapState, mapDispatch)(Signup)
