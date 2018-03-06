ba33faabe22157120a515da82eae962022e4d206
James Kim <vkjames10@gmail.com>
2018-03-06 11:42:50 -0500

    fix(wine-list) quantity management

--- client/components/wine-list.js
+++ client/components/wine-list.js
@@ -6,58 +6,123 @@ import { Link } from 'react-router-dom'
 export const WineList = (props) => {
     const wineList = props.wineListOnProps;
     const user = props.user
-    //console.log(wineList)
-     return (
+    return (
         <div>
             {
-                user.isAdmin && 
-                <div>
-                    <Link to={'/winelist/add-wine'}>Add Wine</Link>
-                </div>
+                user.isAdmin
+                    ?
+                    <div>
+                        <div>
+                            <Link to={'/winelist/add-wine'}>Add Wine</Link>
+                        </div>
+                        <ul>
+                            {wineList.map(wine => {
+                                if (wine.quantity) {
+                                    return (<Link to={`/winelist/${wine.id}`} key={wine.id}>
+                                        <div>
+                                            <img src={wine.img} />
+                                        </div>
+                                        <div>
+                                            <h3>{wine.name}</h3>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                {`${wine.vintage} ${wine.varietal}`}
+                                            </h5>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                {`${wine.place.city}, ${wine.place.state} ${wine.place.country}`}
+                                            </h5>
+                                        </div>
+                                        <div>
+                                            <h6>
+                                                size: {wine.size}
+                                            </h6>
+                                        </div>
+                                        <div>
+                                            <h6>
+                                                price: ${wine.price}
+                                            </h6>
+                                        </div>
+                                    </Link>)
+                                } else {
+                                    return (
+                                        <Link to={`/winelist/${wine.id}`} key={wine.id}>
+                                            <div>
+                                                <h3>{wine.name}</h3>
+                                            </div>
+                                            <div>
+                                                <img src={wine.img} />
+                                            </div>
+                                            <h4>Sold Out</h4>
+                                        </Link>
+                                    )
+                                }
+                            })}
+                        </ul>
+                    </div>
+                    :
+                    <div>
+                        <ul>
+                            {wineList.map(wine => {
+                                if (wine.quantity) {
+                                    return (<Link to={`/winelist/${wine.id}`} key={wine.id}>
+                                        <div>
+                                            <img src={wine.img} />
+                                        </div>
+                                        <div>
+                                            <h3>{wine.name}</h3>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                {`${wine.vintage} ${wine.varietal}`}
+                                            </h5>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                {`${wine.place.city}, ${wine.place.state} ${wine.place.country}`}
+                                            </h5>
+                                        </div>
+                                        <div>
+                                            <h6>
+                                                size: {wine.size}
+                                            </h6>
+                                        </div>
+                                        <div>
+                                            <h6>
+                                                price: ${wine.price}
+                                            </h6>
+                                        </div>
+                                    </Link>)
+                                } else {
+                                    return (
+                                        <div>
+                                            <div>
+                                                <h3>{wine.name}</h3>
+                                            </div>
+                                            <div>
+                                                <img src={wine.img} />
+                                            </div>
+                                            <h4>Sold Out</h4>
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </ul>
+                    </div>
             }
-            <ul>
-                {wineList.map(wine => (
-                    <Link to={`/winelist/${wine.id}`} key={wine.id}>
-                        <div>
-                            <img src={wine.img} />
-                        </div>
-                        <div><h3>{wine.name}</h3></div>
-                        <div>
-                            <h5>
-                                {`${wine.vintage} ${wine.varietal}`}
-                            </h5>
-                        </div>
-                         <div>
-                            <h5>
-                                {`${wine.place.city}, ${wine.place.state} ${wine.place.country}`}
-                            </h5>
-                        </div>
-                        <div>
-                            <h6>
-                                size: {wine.size}
-                            </h6>
-                        </div>
-                        <div>
-                            <h6>
-                                price: ${wine.price}
-                            </h6>
-                        </div>
-                    </Link>
-                ))}
-            </ul>
+
         </div>
     )
 }
 
-
 const mapState = (state) => {
-  //console.log('state: ', state)
     return {
         wineListOnProps: state.wine.wineList,
         user: state.user.loggedInUser
     }
 }
-
 
 const mapDispatch = (dispatch) => {
     return {
@@ -66,6 +131,5 @@ const mapDispatch = (dispatch) => {
         }
     }
 }
-
 
 export default connect(mapState, mapDispatch)(WineList)
